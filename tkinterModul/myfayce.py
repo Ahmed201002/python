42d16ce2982fd2fee2ec2c9aec48bce998628c11
Ahmed Ismail <Ahmed.freiheit@gmail.com>
2021-03-11 12:38:28 +0100

    Delete tkinterModul directory

--- tkinterModul/myfayce.py
+++
@@ -1,6 +0,0 @@
-from tkinter import *
-root=Tk()
-root.title('dudu')
-root.configure(bg='black')
-photo1=photoImage
-root.mainloop()
--- tkinterModul/tkinterdemo2.py
+++
@@ -1,8 +0,0 @@
-import tkinter
-print(tkinter.TkVersion)
-#print(tkinter.TclVersion)
-mainWindow=tkinter.Tk()
-mainWindow.title("Hello World")
-mainWindow.geometry('640x480')
-mainWindow.mainloop()
-
